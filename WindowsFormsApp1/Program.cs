823f1fd61a425059269cbb39b132b2b2c41b1864
Horus Dev <horusm99@gmail.com>
2020-06-12 19:45:26 +0300

    Add Micron ORM. Create connection

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Micron;
 
 namespace WindowsFormsApp1
 {
@@ -14,6 +15,17 @@ namespace WindowsFormsApp1
         [STAThread]
         static void Main()
         {
+            MicronConfig config = new MicronConfig()
+            {
+                DatabaseName = "ngdu",
+                Host = "localhost",
+                User = "root",
+                Password = "",
+                Port = "3306",
+            };
+
+            MicronDbContext.AddConnectionSetup(config);
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Auth());
